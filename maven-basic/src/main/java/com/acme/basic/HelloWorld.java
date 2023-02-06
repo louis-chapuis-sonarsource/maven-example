93cd4d0ffee5944e5971dff3fd8f48b87b428362
Services Team <louis.chapuis@sonarsource.com>
2023-02-06 14:55:17 +0100

    initial commit

---
+++ maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.acme.basic;
+
+public class HelloWorld {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+  
+  void sayBye() {
+	    //System.out.println("Bye World!");
+	  }
+
+}
