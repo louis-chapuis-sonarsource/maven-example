5d7c05b4075bc34311688bad591fccb8c51e8725
louis-chapuis-sonarsource <115462438+louis-chapuis-sonarsource@users.noreply.github.com>
2023-04-18 14:39:41 +0200

    Update HelloWorld.java

--- maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
   void sayHello() {
     System.out.println("Hello World!");
   }
+	
+  void sayHelloHello() {
+    System.out.println("Hello Hello World!");
+  }
+	
 
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
